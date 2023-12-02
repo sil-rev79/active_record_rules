df294c00ea30199ab110fa678933153b9e33171c
Carlo Zancanaro <carlo@zancanaro.id.au>
2023-12-02 20:13:22 +1100

    Refactor to extract Condition#activate out of the rule update entrypoint

--- lib/active_record_rules/condition.rb
+++ lib/active_record_rules/condition.rb
@@ -30,5 +30,62 @@ module ActiveRecordRules
     has_many :condition_activations
     has_many :rules, through: :condition_rules
     validates :match_class, presence: true
+
+    def activate(object)
+      clauses = match_conditions["clauses"]
+      parser = Parser.new.condition_part
+
+      matches = object.persisted? && clauses.all? do |clause|
+        result = case parser.parse(clause)
+                 in { name:, op: "=", rhs: { string: } }
+                   object[name] == string
+                 in { name:, op:, rhs: { string: } }
+                   object[name].public_send(op, string)
+                 in { name:, op: "=", rhs: { number: } }
+                   object[name] == number.to_i
+                 in { name:, op:, rhs: { number: } }
+                   object[name].public_send(op, number.to_i)
+                 in { name:, op: "=", rhs: { boolean: } }
+                   object[name] == (boolean.to_s == "true")
+                 in { name:, op:, rhs: { boolean: } }
+                   object[name].public_send(op, boolean.to_s == "true")
+                 else
+                   true
+                 end
+        logger&.info do
+          if result
+            "Condition(#{id}): #{object.class}(#{object.id}) matches { #{clause} }"
+          else
+            "Condition(#{id}): #{object.class}(#{object.id}) does not match { #{clause} }"
+          end
+        end
+        result
+      end
+
+      if matches
+        logger&.info { "Condition(#{id}): activated by #{object.class}(#{object.id})" }
+        begin
+          condition_activations.create(entry_id: object.id)
+        rescue ActiveRecord::RecordNotUnique => e
+          raise e unless e.message.start_with?("SQLite3::ConstraintException: UNIQUE constraint failed")
+        end
+
+        condition_rules.each do |join|
+          join.rule.activate(join.key, object)
+        end
+      elsif condition_activations.destroy_by(entry_id: object.id).any?
+        logger&.info do
+          if object.persisted?
+            "Condition(#{id}): deactivated for #{object.class}(#{object.id}) - failed a condition check"
+          else
+            "Condition(#{id}): deactivated for #{object.class}(#{object.id}) - was deleted"
+          end
+        end
+
+        condition_rules.each do |join|
+          join.rule.deactivate(join.key, object)
+        end
+      end
+    end
   end
 end
