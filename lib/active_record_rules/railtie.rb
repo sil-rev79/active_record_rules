a2411f97a40815ee922c25b070c26867809fdefd
Carlo Zancanaro <carlo@zancanaro.id.au>
2024-01-19 22:40:19 +1100

    Rename Rake task load_rules => rules:load, and add description

--- lib/active_record_rules/railtie.rb
+++ lib/active_record_rules/railtie.rb
@@ -5,20 +5,27 @@ require "active_record_rules"
 module ActiveRecordRules
   class Railtie < Rails::Railtie
     rake_tasks do
-      task :load_rules do
-        if ActiveRecordRules.automatic_load_paths
-          ActiveRecordRules.load_rules(
-            *ActiveRecordRules.automatic_load_paths.map { Dir[_1] }
-          )
+      namespace :rules do
+        desc "Load rules from configured search paths (automatially run after db:migrate and db:{schema,structure}:load"
+        task load: :environment do
+          if ActiveRecordRules.automatic_load_paths
+            ActiveRecordRules.load_rules(
+              *ActiveRecordRules.automatic_load_paths.map { Dir[_1] }
+            )
+          end
         end
       end
 
       Rake::Task["db:migrate"].enhance do
-        Rake::Task[:load_rules].invoke
+        Rake::Task["rules:load"].invoke
       end
 
       Rake::Task["db:schema:load"].enhance do
-        Rake::Task[:load_rules].invoke
+        Rake::Task["rules:load"].invoke
+      end
+
+      Rake::Task["db:structure:load"].enhance do
+        Rake::Task["rules:load"].invoke
       end
     end
   end
