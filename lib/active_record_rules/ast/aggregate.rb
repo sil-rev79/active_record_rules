0a066ade3b1daaeff7c59a279d155756b364f0c0
Carlo Zancanaro <carlo@zancanaro.id.au>
2024-05-21 12:38:27 +1000

    Fix aggregate subquery to filter, rather than group 😢

--- lib/active_record_rules/ast/aggregate.rb
+++ lib/active_record_rules/ast/aggregate.rb
@@ -30,10 +30,10 @@ module ActiveRecordRules
           grouping = (bindings.keys & query_definer.bindings.keys).map do |name|
             # This is a bit of a cludge to get a value that's valid in
             # the subquery, rather than in the parent query.
-            query_definer.bindings[name].first.call(bindings)
+            "#{bindings[name]} = #{query_definer.bindings[name].first.call(bindings)}"
           end
           sql = query_definer.to_sql(bindings, ["__value"]) # Then we only emit __value here
-          final_result("(#{sql}\n group by #{grouping.join(", ")})")
+          final_result("(#{sql}\n and #{grouping.join(" and ")})")
         end
       end
 
