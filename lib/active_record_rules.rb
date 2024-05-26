fef271b3fa15d00664f7f4b23b3e7a6e61c78783
Carlo Zancanaro <carlo@zancanaro.id.au>
2024-05-26 23:00:43 +1000

    Add comments to methods in top-level ActiveRecordRules module

--- lib/active_record_rules.rb
+++ lib/active_record_rules.rb
@@ -26,8 +26,12 @@ module ActiveRecordRules
     attr_accessor :execution_context
     attr_writer :logger, :dialect
 
+    # Get the current logger. Defaults to ActiveRecord::Base.logger if
+    # no logger is set.
     def logger = @logger || ActiveRecord::Base.logger
 
+    # Get the current SQL dialect to use. Defaults based on the
+    # ActiveRecord adapter for the current connection.
     def dialect
       return @dialect if @dialect
 
@@ -42,6 +46,13 @@ module ActiveRecordRules
       end
     end
 
+    # Load rules from a set of files. The rules will be checked for
+    # duplicates before being loaded into the system.
+    #
+    # The provided filenames will be flattened prior to loading.
+    #
+    # @param filenames [Array<String>] The files to load rules from
+    # @return [Array<Rule>] The rules that were just defined
     def load_rules(*filenames)
       @loaded_rules ||= {}
 
@@ -62,17 +73,22 @@ module ActiveRecordRules
         end
       end
 
-      definitions.each do |_, (definition, _)|
+      definitions.map do |_, (definition, _)|
         define_rule(definition)
       end
-
-      definitions.keys
     end
 
+    # Remove all rules from the in-memory database. This is unlikely
+    # to be what you want.
     def unload_all_rules!
       @loaded_rules = {}
     end
 
+    # Define a new rule by providing a definition, either as a string
+    # or as a parsed Definition object.
+    #
+    # @param definition [String, Definition] The definition of the rule
+    # @return [Rule] The newly defined Rule object.
     def define_rule(definition)
       parsed = definition.is_a?(String) ? Parse.definition(definition) : definition
       rule = Rule.new(definition: parsed)
@@ -88,16 +104,33 @@ module ActiveRecordRules
       rule
     end
 
+    # Retrieve a Rule by name
+    #
+    # @param name [String] The rule name to find
+    # @return [Rule, nil] The rule, or nil if that rule is not defined
     def find_rule(name)
       @loaded_rules[Rule.name_to_id(name)]
     end
 
+    # Remove a Rule definition by name
+    #
+    # @param name [String] The rule name to remove
+    # @return [nil]
     def undefine_rule(name)
       @loaded_rules.delete_if { _2.name == name }
+      nil
     end
 
+    # Process a change in an after_create callback. This captures the
+    # attributes of the object, then activates any relevant rules and
+    # executes the matches.
     def after_create_trigger(record) = after_trigger(capture_create_change(record))
 
+    # Capture the details of attributes in an after_create callback.
+    # This will only capture attributes that are relevant to the rules
+    # currently defined in the system.
+    #
+    # @return A change record suitable to activate rules, or nil if no activation is necessary
     def capture_create_change(record)
       attrs = relevant_attributes(record.class)
       return nil if attrs.empty?
@@ -107,8 +140,16 @@ module ActiveRecordRules
        record.attributes.slice("id", *attrs)]
     end
 
+    # Process a change in an after_update callback. This captures the
+    # attributes of the object, then activates any relevant rules and
+    # executes the matches.
     def after_update_trigger(record) = after_trigger(capture_update_change(record))
 
+    # Capture the details of attributes in an after_update callback.
+    # This will only capture attributes that are relevant to the rules
+    # currently defined in the system.
+    #
+    # @return A change record suitable to activate rules, or nil if no activation is necessary
     def capture_update_change(record)
       attrs = relevant_attributes(record.class)
       return nil if attrs.empty?
@@ -122,8 +163,16 @@ module ActiveRecordRules
       [record.class.name, before, after]
     end
 
+    # Process a change in an after_destroy callback. This captures the
+    # attributes of the object, then activates any relevant rules and
+    # executes the matches.
     def after_destroy_trigger(record) = after_trigger(capture_destroy_change(record))
 
+    # Capture the details of attributes in an after_destroy callback.
+    # This will only capture attributes that are relevant to the rules
+    # currently defined in the system.
+    #
+    # @return A change record suitable to activate rules, or nil if no activation is necessary
     def capture_destroy_change(record)
       attrs = relevant_attributes(record.class)
       return nil if attrs.empty?
@@ -133,21 +182,31 @@ module ActiveRecordRules
        nil]
     end
 
+    # Activate all rules relevant to the provided change.
+    #
+    # @param change The change details to use to activate rules
+    # @return [Array<String>] The ids of RuleMatch records which need execution as a result of this activation
     def activate_rules(change)
       return [] if change.nil?
 
       class_name, previous, current = change
       klass = Object.const_get(class_name)
       @loaded_rules.flat_map do |_, rule|
+        next [] if rule.relevant_attributes_by_class[klass].nil?
+
         pending = rule.calculate_required_activations(klass, previous, current)
-        if pending.any?
-          rule.activate(pending)
-        else
-          []
-        end
+        next [] if pending.empty?
+
+        rule.activate(pending)
       end
     end
 
+    # Execute the code from rule bodies which have been matched, and
+    # need to actually be run. The ids provided to this method should
+    # have come from a call to ActiveRecordRules.activate_rules.
+    #
+    # @param ids [Array] An array of RuleMatch ids which need to be executed
+    # @return nil
     def run_pending_executions(*ids)
       ids.each do |id|
         match = RuleMatch.find_by(id: id)
@@ -162,9 +221,15 @@ module ActiveRecordRules
 
         rule.run_pending_execution(match)
       end
+      nil
     end
     alias run_pending_execution run_pending_executions
 
+    # Activate all rules, for all records. This may generate a *lot*
+    # of ids to process. These ids should then be passed into the
+    # ActiveRecordRules.run_pending_executions method.
+    #
+    # @return [Array<String>] The ids of RuleMatch records which need execution as a result of this activation
     def activate_all
       # This might generate a *lot* of ids to process!
       @loaded_rules.flat_map do |_, rule|
