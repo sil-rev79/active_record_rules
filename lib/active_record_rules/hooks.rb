9d3c8a5716e567e289b9bb86db35b55ef3a3bb4c
Carlo Zancanaro <carlo@zancanaro.id.au>
2024-06-08 23:56:33 +1000

    Fix hooks to handle nils better

--- lib/active_record_rules/hooks.rb
+++ lib/active_record_rules/hooks.rb
@@ -4,27 +4,30 @@ module ActiveRecordRules
   module Hooks
     def self.included(klass)
       klass.after_create do
-        @arr__transaction_changes ||= Set.new
-        change = ActiveRecordRules.capture_create_change(self)
-        ActiveRecordRules.activate_and_execute(change, :after_save)
-        @arr__transaction_changes << change
+        if (change = ActiveRecordRules.capture_create_change(self))
+          @arr__transaction_changes ||= Set.new
+          ActiveRecordRules.activate_and_execute(change, :after_save)
+          @arr__transaction_changes << change
+        end
       end
       klass.after_update do
-        @arr__transaction_changes ||= Set.new
-        change = ActiveRecordRules.capture_update_change(self)
-        ActiveRecordRules.activate_and_execute(change, :after_save)
-        @arr__transaction_changes << change
+        if (change = ActiveRecordRules.capture_update_change(self))
+          @arr__transaction_changes ||= Set.new
+          ActiveRecordRules.activate_and_execute(change, :after_save)
+          @arr__transaction_changes << change
+        end
       end
       klass.after_destroy do
-        @arr__transaction_changes ||= Set.new
-        change = ActiveRecordRules.capture_destroy_change(self)
-        ActiveRecordRules.activate_and_execute(change, :after_save)
-        @arr__transaction_changes << change
+        if (change = ActiveRecordRules.capture_destroy_change(self))
+          @arr__transaction_changes ||= Set.new
+          ActiveRecordRules.activate_and_execute(change, :after_save)
+          @arr__transaction_changes << change
+        end
       end
 
       # Then schedule the rule firings after the transaction commits.
       klass.after_commit do
-        @arr__transaction_changes.each do |change|
+        @arr__transaction_changes&.each do |change|
           ActiveRecordRules.activate_and_execute(change, :after_commit)
           ActiveRecordRules.schedule_async_activation(change)
         end
