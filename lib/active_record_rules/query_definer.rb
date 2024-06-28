666f839c75470437680d5226142267002013a170
Carlo Zancanaro <carlo@zancanaro.id.au>
2024-06-28 23:16:50 +1000

    Write better conditions when one side is known not to be null
    This shouldn't be important, except that not doing it kills Postgres'
    ability to use primary indexes. Doing this avoids sequential scans,
    and generally puts us in a much better place.
    
    This is a hack, but it's needed to solve an immediate problem.

--- lib/active_record_rules/query_definer.rb
+++ lib/active_record_rules/query_definer.rb
@@ -131,8 +131,27 @@ module ActiveRecordRules
       in _, "NULL"
         "#{left} is NULL"
       else
-        "(#{left} = #{right} or (#{left} is null and #{right} is null))"
+        if never_null?(left) || never_null?(right)
+          "#{left} = #{right}"
+        else
+          "(#{left} = #{right} or (#{left} is null and #{right} is null))"
+        end
       end
+    end
+
+    def never_null?(value)
+      value = value.downcase
+      # If the value starts with one of these characters then it's a constant, and thus not null.
+      "0123456789'".include?(value.first) ||
+        # Constant true/false is not null
+        value == "true" ||
+        value == "false" ||
+        # This is hacky: we're assuming that "id" fields are never
+        # null. Ideally this would be based on actually knowing the
+        # database structure so this can work for other fields, too,
+        # but that's too hard right now and this is necessary to get
+        # Postgres to use primary key indexes.
+        value.end_with?(".id")
     end
   end
 end
